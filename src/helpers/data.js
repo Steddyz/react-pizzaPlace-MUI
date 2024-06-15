3fb95fad21caca1f24697274b5e81bfed645923e
Steddy <qwesteddyoff@gmail.com>
2024-06-15 21:18:55 +0300

    first commit
    saving progress

---
+++ src/helpers/data.js
@@ -0,0 +1,56 @@
+export const data = [
+  {
+    name: "Pepperooni",
+    price: "20$",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    image:
+      "https://staticcontent.mypizza.kg/Dishes/Imperiia_Pitstsyi/v1/Pitstsa_40sm/Pepperoni_40sm/Medium.png?hash=92b4aab174ef2fc4e7cb6693e9491501",
+    id: 1,
+  },
+  {
+    name: "Cheese pizza",
+    price: "15$",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    image:
+      "https://staticcontent.mypizza.kg/Dishes/Imperiia_Pitstsyi/v45/Pitstsa_40sm/4_Syira_40sm/Medium.png?hash=f530c2fc1726d73286f6656fd56d59ae",
+    id: 2,
+  },
+  {
+    name: "Pepperooni",
+    price: "20$",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    image:
+      "https://staticcontent.mypizza.kg/Dishes/Imperiia_Pitstsyi/v1/Pitstsa_40sm/Pepperoni_40sm/Medium.png?hash=92b4aab174ef2fc4e7cb6693e9491501",
+    id: 3,
+  },
+  {
+    name: "Pepperooni",
+    price: "20$",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    image:
+      "https://staticcontent.mypizza.kg/Dishes/Imperiia_Pitstsyi/v1/Pitstsa_40sm/Pepperoni_40sm/Medium.png?hash=92b4aab174ef2fc4e7cb6693e9491501",
+    id: 4,
+  },
+  {
+    name: "Cheese pizza",
+    price: "15$",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    image:
+      "https://staticcontent.mypizza.kg/Dishes/Imperiia_Pitstsyi/v45/Pitstsa_40sm/4_Syira_40sm/Medium.png?hash=f530c2fc1726d73286f6656fd56d59ae",
+    id: 5,
+  },
+  {
+    name: "Cheese pizza",
+    price: "15$",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    image:
+      "https://staticcontent.mypizza.kg/Dishes/Imperiia_Pitstsyi/v45/Pitstsa_40sm/4_Syira_40sm/Medium.png?hash=f530c2fc1726d73286f6656fd56d59ae",
+    id: 6,
+  },
+];
---
+++ src/pages/About.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <img
+          src="https://staticcontent.mypizza.kg/Dishes/Imperiia_Pitstsyi/v1/Pitstsa_40sm/Pepperoni_40sm/Medium.png?hash=92b4aab174ef2fc4e7cb6693e9491501"
+          alt="pizza"
+        />
+        <img
+          src="https://staticcontent.mypizza.kg/Dishes/Imperiia_Pitstsyi/v45/Pitstsa_40sm/4_Syira_40sm/Medium.png?hash=f530c2fc1726d73286f6656fd56d59ae"
+          alt="pizza"
+        />
+        <img
+          src="https://staticcontent.mypizza.kg/Dishes/Imperiia_Pitstsyi/v1/Pitstsa_40sm/Pepperoni_40sm/Medium.png?hash=92b4aab174ef2fc4e7cb6693e9491501"
+          alt="pizza"
+        />
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat
+      </p>
+    </div>
+  );
+}
